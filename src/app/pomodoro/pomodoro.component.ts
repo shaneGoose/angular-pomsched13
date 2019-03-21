e10e4f2fa2e29243338c8c7f8dca82fdf080bfd5
shaneGoose <shanegu53@hotmail.com>
2019-03-21 08:54:05 -0400

    3 files modified - base case of work and break established

--- src/app/pomodoro/pomodoro.component.ts
+++ src/app/pomodoro/pomodoro.component.ts
@@ -10,12 +10,13 @@ export class PomodoroComponent implements OnInit {
   private startTime: number;
   private jClock: Date;
   private ticker;
-  private rows: pInterval[];
+  private rows: pInterval[] = [];
   private range: number = 8;  //default 8hr work day
   private workCounter: number = 0;
   private shortCounter: number = 0;
   private longCounter: number = 0;
-  private cycleCounter: number = 0;
+  private lunchCounter: number = 0;
+  private fortyfiveCounter: number = 0;
 
   constructor() {}
 
@@ -24,7 +25,6 @@ export class PomodoroComponent implements OnInit {
     this.jClock = new Date(this.startTime);
     this.ticker = setInterval(this.tick, 500);
     this.makeSchedule(8);
-    //alert("This Ran");
   }
 
   ngOnDestroy() {
@@ -34,18 +34,45 @@ export class PomodoroComponent implements OnInit {
   //One shot: will not run unless invoked by form
   makeSchedule = (length: number) => {
     if (length < 6 || length > 16) {throw 'Illegal Argument Exception! Work hours must be between 6 and 16!'}
-
+    /*
     const SHORT_MAX = Math.ceil(Math.sqrt(length));
     const WORK_MAX = SHORT_MAX + 1;
     let accum = 0;
     let id = 1;
+    */
     
-    //Test field
-    let temp: pInterval = {id: 1, time: new Date(this.startTime), interval: 'work'};
+    //Test field    
+
+    //Start building the table here
+    let id = 1;
+    let shiftCounter = 0;
+    let runTime = this.startTime
+    let temp: pInterval = {id: id++, time: new Date(runTime), interval: 'work'};
+    runTime = runTime + WORK;
+    this.workCounter++;
+    let accum = WORK;
     this.rows.push(temp);
-    temp = {id: 2, time: new Date(this.startTime + WORK), interval: 'short break'};
-    this.rows.push(temp);
-    alert("this ran");
+    do {
+      //Enter with [1, 0, 0, 0, 0] state
+      //Make base with alternate work and break then modify on the back end
+      if ((this.sumCounters() % length) % 2 === 0) {
+        temp = {id: id++, time: new Date(runTime), interval: 'break'};
+        this.rows.push(temp);
+        runTime = runTime + SHORT;
+        accum = accum + SHORT;
+        this.shortCounter++;
+      }
+      else if ((this.sumCounters() % length) % 2 === 1) {
+        temp = {id: id++, time: new Date(runTime), interval: 'work'};
+        this.rows.push(temp);
+        runTime = runTime + WORK;
+        accum = accum + WORK;
+        this.workCounter++;
+      }
+
+      //Backend mods, check the last entry and make mods for long break, lunch, and special cases
+
+    } while (accum < length * 60 * MIN_TO_MS)
   }
 
   //Ticker function
@@ -55,7 +82,7 @@ export class PomodoroComponent implements OnInit {
 
   //Helper functions
   sumCounters = ():number => {
-    return this.longCounter + this.shortCounter + this.workCounter;
+    return this.longCounter + this.shortCounter + this.workCounter + this.lunchCounter + this.fortyfiveCounter;
   }
 }
 
@@ -65,8 +92,9 @@ interface pInterval {
   interval: string
 }
 
-const MS_CONV = 60 * 1000;
-const WORK = 25 * MS_CONV;
-const SHORT = 5 * MS_CONV;
-const LONG = 15 * MS_CONV;
-const LUNCH = 30 * MS_CONV;
+const MIN_TO_MS = 60 * 1000;
+const WORK = 25 * MIN_TO_MS;
+const SHORT = 5 * MIN_TO_MS;
+const LONG = 15 * MIN_TO_MS;
+const LUNCH = 30 * MIN_TO_MS;
+const FORTYFIVE = 45 * MIN_TO_MS;
