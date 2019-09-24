139cbe886538de528ed94c054f57edd72db4f166
shaneGoose <shanegu53@hotmail.com>
2019-09-24 10:26:53 -0400

    change 'my-app' to 'pom-app'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'pom-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
